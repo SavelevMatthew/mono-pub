2ac15acd155d2b01198f51e3919b152ec965ae7d
SavelevMatthew <savelevmatthew@gmail.com>
2023-05-12 14:51:47 +0500

    build(mono-pub): added version utils export

--- packages/mono-pub/rollup.config.mjs
+++ packages/mono-pub/rollup.config.mjs
@@ -3,8 +3,10 @@ import { filePipe, typePipe } from '@mono-pub/configs/rollup'
 /** @typedef import('rollup').RollupOptions RollupOptions */
 /** @type {Array<RollupOptions>} */
 const options = [
-    filePipe('src/index.ts', 'dist/index.cjs', 'dist/index.mjs', { clean: true, withTypes: true }),
-    typePipe('dist/dts/index.d.ts', 'dist/index.d.ts', { cleanTypes: true }),
+    filePipe('src/utils/index.ts', 'dist/utils.cjs', 'dist/utils.mjs', { clean: true, withTypes: true }),
+    filePipe('src/index.ts', 'dist/index.cjs', 'dist/index.mjs', { withTypes: true }),
+    typePipe('dist/dts/index.d.ts', 'dist/index.d.ts'),
+    typePipe('dist/dts/utils/index.d.ts', 'dist/utils.d.ts', { cleanTypes: true }),
 ]
 
 export default options
---
+++ packages/mono-pub/src/utils/index.ts
@@ -0,0 +1,1 @@
+export { versionToString } from './versions'
