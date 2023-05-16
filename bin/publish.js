69631be501fb1d29a0f30c2aa0a9bb41ad962ea5
SavelevMatthew <savelevmatthew@gmail.com>
2023-05-16 14:20:48 +0500

    build(github): added release script

---
+++ bin/publish.js
@@ -0,0 +1,42 @@
+const execa = require('execa')
+const publish = require('mono-pub')
+const git = require('@mono-pub/git')
+const github = require('@mono-pub/github')
+const npm = require('@mono-pub/npm')
+const commitAnalyzer = require('@mono-pub/commit-analyzer')
+
+const builder = {
+    name: '@mono-pub/local-builder',
+    async prepare(_, ctx) {
+        await execa('yarn', ['build'], { cwd: ctx.cwd })
+    },
+}
+
+const BREAKING_KEYWORDS = ['BREAKING CHANGE', 'BREAKING-CHANGE', 'BREAKING CHANGES', 'BREAKING-CHANGES']
+
+publish(
+    ['packages/configs'],
+    [
+        git(),
+        github({
+            extractCommitsFromSquashed: true,
+            releaseNotesOptions: {
+                rules: [
+                    { breaking: true, section: '⚠️ BREAKING CHANGES' },
+                    { type: 'feat', section: '🦕 New features' },
+                    { type: 'fix', section: '🐞 Bug fixes' },
+                    { type: 'perf', section: '🚀 Performance increases' },
+                    { dependency: true, section: '🌐Dependencies' },
+                ],
+                breakingNoteKeywords: BREAKING_KEYWORDS,
+            },
+        }),
+        commitAnalyzer({
+            minorTypes: ['feat'],
+            patchTypes: ['perf', 'fix'],
+            breakingNoteKeywords: BREAKING_KEYWORDS,
+        }),
+        builder,
+        npm(),
+    ]
+)
