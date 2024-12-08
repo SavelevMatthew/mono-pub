8124eedc2d8c169f54c603f477cf9c1bd3f57ad3
Matthew <50069872+SavelevMatthew@users.noreply.github.com>
2024-12-09 00:23:10 +0500

    refactor(global): migrate rollup build to tsup (#25)
    * feat(configs)!: rollup replaced with tsup for speed and simplicity
    
    * refactor(global): migrate rollup build to tsup
    
    * chore(global): sync yarn.lock
    
    * chore(commit-analyzer): remove redundant rollup file
    
    * chore(mono-pub): removed declarationsDir from ts config
    
    * chore(mono-pub): fixed semgrep issues

---
+++ packages/configs/tsup.mjs
@@ -0,0 +1,36 @@
+import path from 'path'
+import { lstatSync, existsSync } from 'fs'
+import { defineConfig } from 'tsup'
+
+const ALLOWED_EXTENSIONS = ['.ts']
+
+function _getInputs(exports) {
+    return Object.keys(exports).map((relativeImport) => {
+        // NOTE: path utils are expected and part of logic here
+        // nosemgrep: javascript.lang.security.audit.path-traversal.path-join-resolve-traversal.path-join-resolve-traversal
+        const srcPath = path.join('src', relativeImport)
+        const isDirectory = existsSync(srcPath) && lstatSync(srcPath).isDirectory()
+
+        // NOTE: path utils are expected and part of logic here
+        // nosemgrep: javascript.lang.security.audit.path-traversal.path-join-resolve-traversal.path-join-resolve-traversal
+        const pathToFile = isDirectory ? path.join(srcPath, 'index') : srcPath
+
+        const ext = ALLOWED_EXTENSIONS.find((extension) => existsSync(`${pathToFile}${extension}`))
+        if (!ext) throw new Error(`No allowed extensions found for file: ${relativeImport}`)
+
+        return `${pathToFile}${ext}`
+    })
+}
+
+export function createBuildPipeline(exports) {
+    return defineConfig({
+        entry: _getInputs(exports),
+        clean: true,
+        dts: true,
+        sourcemap: true,
+        format: ['cjs', 'esm'],
+        target: 'node16',
+        minify: true,
+        splitting: false,
+    })
+}
